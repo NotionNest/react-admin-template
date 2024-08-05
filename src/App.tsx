0a3793cb2cdc6ee95dd1f13d8642bb01eb3bbcf5
aiden <danyang.aiden@outlook.com>
2024-08-05 23:55:23 +0800

    feat: eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './theme/index.css';
+import './theme/index.css'
 
 export default function App() {
+  console.log('cdad')
   return (
-    <div className=" ">
+    <div className="">
       <h1 className="bg-green text-basic-600">Hello world!</h1>
-      <div className="border-yellow-600 bg-amber-700 border">1212</div>
+      <div className="border border-yellow-600 bg-amber-700">1212</div>
     </div>
-  );
+  )
 }
